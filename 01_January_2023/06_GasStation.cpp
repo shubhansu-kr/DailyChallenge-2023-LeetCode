c4cd7fdc3d225bbaa2bb8caafd8d9cb3142cae39
Shubhansu kumar <shubhansu2021@gmail.com>
2023-01-07 20:13:41 +0530

    Gas Station: Find the starting point
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 01_January_2023/06_GasStation.cpp
+++ 01_January_2023/06_GasStation.cpp
@@ -3,7 +3,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n = gas.size();
+        int total_surplus = 0;
+        int surplus = 0;
+        int start = 0;
+        
+        for(int i = 0; i < n; i++){
+            total_surplus += gas[i] - cost[i];
+            surplus += gas[i] - cost[i];
+            if(surplus < 0){
+                surplus = 0;
+                start = (i + 1)%n;
+            }
+        }
+        return (total_surplus < 0) ? -1 : start;
+    }
+};
 
 class Solution
 {
