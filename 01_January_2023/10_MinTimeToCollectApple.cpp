2882d460c89d800c6f0e1e12b5ffb25a65b86573
Shubhansu kumar <shubhansu2021@gmail.com>
2023-01-11 20:19:51 +0530

    Min. Time To collect apples on the tree
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 01_January_2023/10_MinTimeToCollectApple.cpp
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/minimum-time-to-collect-all-apples-in-a-tree/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    unordered_map<int, vector<int>> g;
+    unordered_map<int, bool> visited;
+
+    void createGraph(vector<vector<int>> &edges)
+    {
+        for (auto e : edges)
+        {
+            g[e[0]].push_back(e[1]);
+            g[e[1]].push_back(e[0]);
+        }
+    }
+
+    int dfs(int node, int myCost, vector<bool> &hasApple)
+    {
+        if (visited[node]) return 0;
+        visited[node] = true;
+        int childrenCost = 0;
+        for (auto x : g[node]) childrenCost += dfs(x, 2, hasApple);
+        if (childrenCost == 0 && hasApple[node] == false) return 0;
+
+        return (childrenCost + myCost);
+    }
+
+    int minTime(int n, vector<vector<int>> &edges, vector<bool> &hasApple)
+    {
+        createGraph(edges);
+        return dfs(0, 0, hasApple);
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
