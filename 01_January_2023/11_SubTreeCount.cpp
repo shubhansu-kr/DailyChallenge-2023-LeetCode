1afaf50a67e15ca92c9f28aae7b6099a91c060cd
Shubhansu kumar <shubhansu2021@gmail.com>
2023-01-12 21:41:34 +0530

    WA
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 01_January_2023/11_SubTreeCount.cpp
+++ 01_January_2023/11_SubTreeCount.cpp
@@ -5,6 +5,33 @@ using namespace std;
 
 class Solution
 {
+    // Wrong answer 
+    // 4 [[0,2],[0,3],[1,2]] "aeed"
+public:
+    vector<vector<int>> createAdj(vector<vector<int>> &edges){
+        int n = edges.size()+1;
+        vector<vector<int>> adj(n, vector<int>(n, 0));
+        for(auto &it: edges) adj[it[0]][it[1]] = 1;
+        return adj;
+    }
+
+    int countSubTree(vector<vector<int>> &adj, char &label, string &labels, int &num) {
+        int count = label == labels[num];
+        for (int i = 0; i < adj.size(); ++i) if (adj[num][i]) count += countSubTree(adj, label, labels, i);
+        return count;
+    }
+
+    vector<int> countSubTrees(int n, vector<vector<int>> &edges, string labels)
+    {
+        vector<vector<int>> adj = createAdj(edges);
+        vector<int> ans(n, 0); 
+        for (int i = 0; i < n; ++i) ans[i] = countSubTree(adj, labels[i], labels, i);
+        return ans;
+    }
+};
+
+class Solution
+{
     // Wrong Answer: 
     // 7 [[0,1],[1,2],[2,3],[3,4],[4,5],[5,6]] "aaabaaa"
 
