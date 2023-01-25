3f8223416cab385f5488815a70ffc024ed759b00
Shubhansu kumar <shubhansu2021@gmail.com>
2023-01-25 20:46:20 +0530

    Find the closest common node
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 01_January_2023/24_ClosestCommonNode.cpp
+++ 01_January_2023/24_ClosestCommonNode.cpp
@@ -4,6 +4,33 @@
 using namespace std ;
 
 class Solution {
+public:
+    int closestMeetingNode(vector<int>& edges, int node1, int node2) {
+        int n = edges.size();
+        vector<int> visited(n, -1);
+
+        for (int i = 0; i < n; ++i)
+        {
+            if (node1 == node2) 
+                return node1;
+            else if (visited[node1] == 2 && visited[node2] == 1)
+                return min(node1, node2);
+            else if (visited[node2] == 1) 
+                return node2;
+            else if (visited[node1] == 2) 
+                return node1;
+
+            visited[node1] = 1;
+            visited[node2] = 2;
+
+            if (edges[node1] != -1) node1 = edges[node1];
+            if (edges[node2] != -1) node2 = edges[node2];
+        }
+        return -1;
+    }
+};
+
+class Solution {
     // WA
     // edges = [2,2,3,-1], node1 = 0, node2 = 1
 public:
@@ -31,6 +58,7 @@ public:
 };
 
 int main () {
-    
+    // TestCase : 
+    // [1, 2, 3, 5, 5, 6, 4] [2, 4]
     return 0;
 }
