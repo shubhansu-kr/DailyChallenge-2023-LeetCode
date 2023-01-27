85fb6ae82c36e1e1b315b25938a3ab6d5c897dc8
Shubhansu kumar <shubhansu2021@gmail.com>
2023-01-27 20:27:12 +0530

    Concatinated Word
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 01_January_2023/26_ConcatinatedWord.cpp
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/concatenated-words/
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+public:
+    vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
+        unordered_set<string> words_set;
+        for (string word : words) words_set.insert(word);
+        vector<string> res;
+    
+        for (string word : words) {
+            int n = word.size();
+            vector<int> dp(n + 1, 0);
+            dp[0] = 1;
+            for (int i = 0; i < n; i++) {
+                if (!dp[i]) continue;
+                for (int j = i + 1; j <= n; j++) {
+                    if (j - i < n && words_set.count(word.substr(i, j - i))) {
+                        dp[j] = 1;
+                    }
+                }
+                if (dp[n]) {
+                    res.push_back(word);
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+};
+
+int main () {
+    
+    return 0;
+}
