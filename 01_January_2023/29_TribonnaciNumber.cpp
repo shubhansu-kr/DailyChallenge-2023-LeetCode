b8e66508adafe12178ec3329f144a59a63304bd8
Shubhansu kumar <shubhansu2021@gmail.com>
2023-01-30 09:05:08 +0530

    Solution2: Tabulation
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 01_January_2023/29_TribonnaciNumber.cpp
+++ 01_January_2023/29_TribonnaciNumber.cpp
@@ -3,6 +3,21 @@
 #include <bits/stdc++.h>
 using namespace std ;
 
+class Solution2 {
+    // Tabulation
+public:
+    int tribonacci(int n) {
+        if (n < 3) return n != 0;
+        vector<int> dp(n+1, -1);
+        dp[0] = 0, dp[1] = 1, dp[2] = 1;
+        for (int i = 3; i < n+1; ++i)
+        {
+            dp[i] = dp[i-1]+dp[i-2]+dp[i-3];
+        }
+        return dp[n];
+    }
+};
+
 class Solution1 {
     // Recursion: Memoization
 public:
