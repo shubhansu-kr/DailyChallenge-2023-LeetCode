fd7f7002e7099c474c8dc5570ff4016c2c6e2604
Shubhansu kumar <shubhansu2021@gmail.com>
2023-01-30 09:01:18 +0530

    Solution1: Memoizaation
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 01_January_2023/29_TribonnaciNumber.cpp
+++ 01_January_2023/29_TribonnaciNumber.cpp
@@ -3,6 +3,21 @@
 #include <bits/stdc++.h>
 using namespace std ;
 
+class Solution1 {
+    // Recursion: Memoization
+public:
+    int tribonacci(int n) {
+        vector<int> dp(n+1, -1);
+        return dp[n] = solve(n, dp);
+    }
+
+    int solve(int n, vector<int> &dp){
+        if (n < 3) return dp[n] = n != 0;
+        if (dp[n] != -1) return dp[n];
+        return dp[n] = solve(n-1, dp)+solve(n-2, dp)+solve(n-3, dp);
+    }
+};
+
 class Solution {
     // Recursion: BruteForce
 public:
