a6435f96901d459d84569b959a8faa862c20f7da
Shubhansu kumar <shubhansu2021@gmail.com>
2023-01-31 21:09:01 +0530

    Find the team with best score
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 01_January_2023/30_BestTeamScores.cpp
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/best-team-with-no-conflicts/
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+public:
+    int bestTeamScore(vector<int>& scores, vector<int>& ages) {
+        vector<pair<int, int>> players;
+        int n = scores.size();
+        for (int i=0; i<n; i++) {
+            players.push_back({ages[i], scores[i]});
+        }
+        sort(players.begin(), players.end(), greater<>());
+        
+        int ans = 0;
+        vector<int> dp(n);
+        for (int i=0; i<n; i++) {
+            int score = players[i].second;
+            dp[i] = score;
+            for (int j=0; j<i; j++) {
+                if (players[j].second >= players[i].second) {
+                    dp[i] = max(dp[i], dp[j] + score);
+                }
+            }
+            ans = max(ans, dp[i]);
+        }
+        return ans;
+    }
+};
+
+int main () {
+    
+    return 0;
+}
