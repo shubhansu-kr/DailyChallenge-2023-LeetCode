c921af17415cebb4d518aecb1ce8d7b4896d662b
Shubhansu kumar <shubhansu2021@gmail.com>
2023-02-09 22:39:55 +0530

    Name the Company
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 02_February_2023/08_NamingCompany.cpp
+++ 02_February_2023/08_NamingCompany.cpp
@@ -1,12 +1,39 @@
 // https://leetcode.com/problems/naming-a-company/
 
 #include <bits/stdc++.h>
-using namespace std ;
+using namespace std;
 
-class Solution {
-    // BruteForce: TLE : Need Optimisation. 
+class Solution
+{
 public:
-    long long distinctNames(vector<string>& ideas) {
+    long long distinctNames(vector<string> &ideas)
+    {
+        unordered_set<string> count[26];
+        for (auto &s : ideas)
+            count[s[0] - 'a'].insert(s.substr(1));
+        long long res = 0;
+        for (int i = 0; i < 26; ++i)
+            for (int j = i + 1; j < 26; ++j)
+            {
+                long long c1 = 0L, c2 = 0L;
+                for (auto &c : count[i])
+                    if (!count[j].count(c))
+                        c1++;
+                for (auto &c : count[j])
+                    if (!count[i].count(c))
+                        c2++;
+                res += c1 * c2;
+            }
+        return res * 2;
+    }
+};
+
+class Solution
+{
+    // BruteForce: TLE : Need Optimisation.
+public:
+    long long distinctNames(vector<string> &ideas)
+    {
         unordered_set<string> id(ideas.begin(), ideas.end());
 
         // Possible combination
@@ -18,9 +45,10 @@ public:
             for (int j = 0; j < ideas.size(); ++j)
             {
                 // Can't match identical strings.
-                if (i == j) continue;
+                if (i == j)
+                    continue;
 
-                // Check ideas[i], ideas[j]; for valid combination. 
+                // Check ideas[i], ideas[j]; for valid combination.
                 string b = ideas[j];
 
                 // Swap the first char of each string
@@ -28,15 +56,16 @@ public:
                 b[0] = ideas[i][0];
 
                 // Check availability in ideas.
-                if (!id.count(a) && !id.count(b)) ++ans;
+                if (!id.count(a) && !id.count(b))
+                    ++ans;
             }
         }
         return ans;
-
     }
 };
 
-int main () {
-    
+int main()
+{
+
     return 0;
 }
