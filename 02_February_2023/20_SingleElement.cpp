320620397713000e33ab0ad122c4efe245d92c09
Shubhansu kumar <shubhansu2021@gmail.com>
2023-02-21 16:18:51 +0530

    Solution1: BinarySearch
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 02_February_2023/20_SingleElement.cpp
+++ 02_February_2023/20_SingleElement.cpp
@@ -4,6 +4,30 @@
 using namespace std ;
 
 class Solution {
+    // 
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        // Using binary split.
+        int low = 0, high = nums.size()-1;
+        int mid = (low + high) / 2;
+        while (low < high)
+        {
+            mid = (low + high)/2;
+            if (nums[mid] == nums[mid-1]){
+                high = mid-1;
+            }
+            else if (nums[mid] == nums[mid+1]){
+                low = mid+1;
+            }
+            else {
+                return nums[mid];
+            }
+        }
+        return nums[mid];
+    }
+};
+
+class Solution {
     // BruteForce: Xor operator. // O(n) 
 public:
     int singleNonDuplicate(vector<int>& nums) {
