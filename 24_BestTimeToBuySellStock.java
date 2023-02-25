cacf72fd89c56250f37741f111593d12460d2905
Shubhansu kumar <shubhansu2021@gmail.com>
2023-02-25 20:23:48 +0530

    Java: Best time to buy and sell stock
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 24_BestTimeToBuySellStock.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int ans = -1, mini = prices[0];
+        
+        for (int i = 1; i < prices.length; i++) {
+            ans = Math.max(ans, prices[i]-mini);
+            mini = Math.min(mini, prices[i]);
+        }
+
+        return ans > 0 ? ans : 0;
+    }
+}
