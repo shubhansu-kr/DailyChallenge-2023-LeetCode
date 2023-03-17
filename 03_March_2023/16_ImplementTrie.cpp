aad804c4c147a32b24ee9096a81c73e3ffbc7556
Shubhansu kumar <shubhansu2021@gmail.com>
2023-03-17 19:58:39 +0530

    Implement Trie class
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 03_March_2023/16_ImplementTrie.cpp
@@ -0,0 +1,45 @@
+// https://leetcode.com/problems/implement-trie-prefix-tree/
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Trie {
+public:
+    Trie() {}
+
+    void insert(string word) {
+        Trie* node = this;
+        for (char ch : word) {
+            if (!node->next.count(ch)) { node->next[ch] = new Trie(); }
+            node = node->next[ch];
+        }
+        node->isword = true;
+    }
+
+    bool search(string word) {
+        Trie* node = this;
+        for (char ch : word) {
+            if (!node->next.count(ch)) { return false; }
+            node = node->next[ch];
+        }
+        return node->isword;
+    }
+
+    bool startsWith(string prefix) {
+        Trie* node = this;
+        for (char ch : prefix) {
+            if (!node->next.count(ch)) { return false; }
+            node = node->next[ch];
+        }
+        return true;
+    }
+
+private:
+    unordered_map<char, Trie*> next = {};
+    bool isword = false;
+};
+
+int main () {
+    
+    return 0;
+}
