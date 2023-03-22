9abf9e524d3227790a5456f7bb0c5097ebff7593
Shubhansu kumar <shubhansu2021@gmail.com>
2023-03-22 21:07:47 +0530

    Zeroes filled subarray
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 03_March_2023/20_ZeroesFilledSubarr.cpp
+++ 03_March_2023/20_ZeroesFilledSubarr.cpp
@@ -1,22 +1,26 @@
 // https://leetcode.com/problems/number-of-zero-filled-subarrays/
 
 #include <bits/stdc++.h>
-using namespace std ;
+using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    long long zeroFilledSubarray(vector<int>& nums) {
-    long long res = 0;
-    for (int i = 0, j = 0; i < nums.size(); ++i) {
-        if (nums[i] != 0)
-            j = i + 1;
-         res += i - j + 1;
+    long long zeroFilledSubarray(vector<int> &nums)
+    {
+        long long res = 0;
+        for (int i = 0, j = 0; i < nums.size(); ++i)
+        {
+            if (nums[i] != 0)
+                j = i + 1;
+            res += i - j + 1;
+        }
+        return res;
     }
-    return res;
-}
 };
 
-int main () {
-    
+int main()
+{
+
     return 0;
 }
