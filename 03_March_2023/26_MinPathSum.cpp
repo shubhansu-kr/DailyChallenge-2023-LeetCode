4db7bf6e402e01895b7dc18bdace169d01124f9d
Shubhansu kumar <shubhansu2021@gmail.com>
2023-03-27 20:48:22 +0530

    Find the min. Path sum
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 03_March_2023/26_MinPathSum.cpp
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/minimum-path-sum/
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for (int i = 0; i < m; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                if (i == 0 && j == 0)
+                {
+                    dp[i][j] = grid[i][j];
+                }
+                else
+                {
+                    int up = grid[i][j];
+                    if (i > 0)
+                        up += dp[i - 1][j];
+                    else
+                        up += 1e9;
+                    int left = grid[i][j];
+                    if (j > 0)
+                        left += dp[i][j - 1];
+                    else
+                        left += 1e9;
+
+                    dp[i][j] = min(left, up);
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+};
+
+int main () {
+    
+    return 0;
+}
