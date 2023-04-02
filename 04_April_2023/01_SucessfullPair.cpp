88078d6a90ea55bda0a584a9764d371650bb43c3
Shubhansu kumar <shubhansu2021@gmail.com>
2023-04-02 21:26:07 +0530

    Successfull Pair
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 04_April_2023/01_SucessfullPair.cpp
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/successful-pairs-of-spells-and-potions/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> successfulPairs(vector<int> &spells, vector<int> &potions, long long success)
+    {
+        sort(potions.begin(), potions.end());
+        vector<int> res;
+        for (int a : spells)
+        {
+            long need = (success + a - 1) / a;
+            auto it = lower_bound(potions.begin(), potions.end(), need);
+            res.push_back(potions.end() - it);
+        }
+        return res;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
