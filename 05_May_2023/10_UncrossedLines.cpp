7670191f206d198d2817c9d2666afc421b074f6f
Shubhansu kumar <shubhansu2021@gmail.com>
2023-05-11 16:25:19 +0530

    Uncrossed Lines : Longest Common subsequence
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 05_May_2023/10_UncrossedLines.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/uncrossed-lines/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int maxUncrossedLines(vector<int> &nums1, vector<int> &nums2)
+    {
+        int m = nums1.size(), n = nums2.size(), dp[m + 1][n + 1];
+        memset(dp, 0, sizeof(dp));
+        for (int i = 1; i <= m; ++i)
+            for (int j = 1; j <= n; ++j)
+                dp[i][j] = nums1[i - 1] == nums2[j - 1] ? dp[i - 1][j - 1] + 1 : max(dp[i][j - 1], dp[i - 1][j]);
+        return dp[m][n];
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
