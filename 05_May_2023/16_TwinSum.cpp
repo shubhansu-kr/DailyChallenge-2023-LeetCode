bef3ec170ac50489825989c99be3fd89f64c350d
Shubhansu kumar <shubhansu2021@gmail.com>
2023-05-17 21:13:24 +0530

    Find the sum of twin pair
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 05_May_2023/16_TwinSum.cpp
@@ -0,0 +1,54 @@
+// https://leetcode.com/problems/maximum-twin-sum-of-a-linked-list/
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+struct ListNode {
+    int val;
+    ListNode * next;
+    ListNode():val(0), next(nullptr){}
+    ListNode(int val):val(val), next(nullptr){}
+    ListNode(int val, ListNode *next):val(val), next(next){}
+};
+
+class Solution {
+public:
+    int pairSum(ListNode* head) {
+        ListNode *slow = head;
+        ListNode *fast = head;
+        int maxVal = 0;
+
+        // Get middle of linked list
+        while (fast && fast->next)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+
+        // Reverse second part of linked list
+        ListNode *nextNode, *prev = NULL;
+        while (slow)
+        {
+            nextNode = slow->next;
+            slow->next = prev;
+            prev = slow;
+            slow = nextNode;
+        }
+
+        // Get max sum of pairs
+        while (prev)
+        {
+            maxVal = max(maxVal, head->val + prev->val);
+            prev = prev->next;
+            head = head->next;
+        }
+
+        return maxVal;
+        
+    }
+};
+
+int main () {
+    
+    return 0;
+}
