3143b5f351e2dd04e5a74b1a2137e8d568aa73cc
Shubhansu kumar <shubhansu2021@gmail.com>
2023-05-23 19:13:06 +0530

    Stream Kth Largest Elemetnt
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 05_May_2023/23_StreamKthLargest.cpp
@@ -0,0 +1,42 @@
+// https://leetcode.com/problems/kth-largest-element-in-a-stream/description/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class KthLargest
+{
+public:
+    //     Discussion Solution
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int size;
+    KthLargest(int k, vector<int> &nums)
+    {
+        size = k;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            pq.push(nums[i]);
+            if (pq.size() > k)
+                pq.pop();
+        }
+    }
+
+    int add(int val)
+    {
+        pq.push(val);
+        if (pq.size() > size)
+            pq.pop();
+        return pq.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
+
+int main()
+{
+
+    return 0;
+}
