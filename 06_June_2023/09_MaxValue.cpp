767a7b7a662abd00210a3105b76687f2ede92994
Shubhansu kumar <shubhansu2021@gmail.com>
2023-06-10 12:39:20 +0530

    Find the max value for given index
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 06_June_2023/09_MaxValue.cpp
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/maximum-value-at-a-given-index-in-a-bounded-array/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int maxValue(int n, int index, int maxSum)
+    {
+        maxSum -= n;
+        int left = 0, right = maxSum, mid;
+        while (left < right)
+        {
+            mid = (left + right + 1) / 2;
+            if (test(n, index, mid) <= maxSum) left = mid;
+            else right = mid - 1;
+        }
+        return left + 1;
+    }
+
+    long test(int n, int index, int a)
+    {
+        int b = max(a - index, 0);
+        long res = long(a + b) * (a - b + 1) / 2;
+        b = max(a - ((n - 1) - index), 0);
+        res += long(a + b) * (a - b + 1) / 2;
+        return res - a;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
