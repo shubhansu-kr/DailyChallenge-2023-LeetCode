096ea3378d00a8540c0fae848c8ca19ea83ac042
Shubhansu kumar <shubhansu2021@gmail.com>
2023-06-26 18:34:35 +0530

    Find the min. cost to hire workers
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- test.cpp
+++ test.cpp
@@ -1,32 +1,10 @@
-// Let's Go
+// Let's go
 
 #include <bits/stdc++.h>
-using namespace std;
+using namespace std ;
 
-class Solution
-{
-public:
-    string findOdd(vector<string> series)
-    {
-    }
-
-    string findOdd(vector<string> &series)
-    {
-        for (int j = 0; j + 1 < series[0].size(); ++j)
-        {
-            unordered_map<int, vector<int>> m;
-            for (int i = 0; i < series.size(); ++i)
-                if (m[series[i][j + 1] - series[i][j]].size() < 2)
-                    m[series[i][j + 1] - series[i][j]].push_back(i);
-            if (m.size() == 2)
-                return series[begin(m)->second.size() == 1 ? begin(m)->second.back() : next(begin(m))->second.back()];
-        }
-        return series[0];
-    }
-};
-
-int main()
-{
-
+int main () {
+    
+    
     return 0;
 }
