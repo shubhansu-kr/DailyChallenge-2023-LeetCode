ee5d51dbfe9cbcc61a1d1bd39bb0b53c6fe5630f
Shubhansu kumar <shubhansu2021@gmail.com>
2023-08-08 21:45:59 +0530

    Find the target
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 08_August_2023/07_SearchInRotatedSortedArray.cpp
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+        int lo = 0, hi = nums.size();
+        while (lo < hi)
+        {
+            int mid = (lo + hi) / 2;
+
+            double num = (nums[mid] < nums[0]) == (target < nums[0])
+                             ? nums[mid]
+                         : target < nums[0] ? -INFINITY
+                                            : INFINITY;
+
+            if (num < target)
+                lo = mid + 1;
+            else if (num > target)
+                hi = mid;
+            else
+                return mid;
+        }
+        return -1;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
