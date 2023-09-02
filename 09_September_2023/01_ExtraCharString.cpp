ceeb2d541c8c2fb9412f810537e57d6f73b5e5a5
Shubhansu kumar <shubhansu2021@gmail.com>
2023-09-02 21:38:41 +0530

    Extra Char in the string
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 09_September_2023/01_ExtraCharString.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/extra-characters-in-a-string/description/
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+public:
+    int minExtraChar(string s, vector<string>& dictionary) {
+        int max_val = s.length() + 1;
+        vector<int> dp(s.length() + 1, max_val);
+        dp[0] = 0;
+
+        unordered_set<string> dictionary_set(dictionary.begin(), dictionary.end());
+
+        for (int i = 1; i <= s.length(); ++i) {
+            dp[i] = dp[i - 1] + 1;
+            for (int l = 1; l <= i; ++l) {
+                if (dictionary_set.find(s.substr(i - l, l)) != dictionary_set.end()) {
+                    dp[i] = min(dp[i], dp[i - l]);
+                }
+            }
+        }
+        return dp.back();
+    }
+};
+
+int main () {
+    
+    return 0;
+}
