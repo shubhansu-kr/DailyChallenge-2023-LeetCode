b464477f492faf613a46a59290894355ce00cd81
Shubhansu kumar <shubhansu2021@gmail.com>
2023-10-02 23:13:29 +0530

    Test
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- test.cpp
+++ test.cpp
@@ -3,6 +3,42 @@
 #include <bits/stdc++.h>
 using namespace std ;     
 
+class Solution {
+public:
+    string reverseWords(string s) 
+    {
+           
+        while(s.size() > 0 && s.back() == ' ') s.pop_back();
+        reverse(begin(s), end(s));
+        while(s.size() > 0 && s.back() == ' ') s.pop_back();
+        int i, start, end;
+        start = end = -1; i = 0;
+        int n = s.length();
+
+        for (i = 0; i <= n; i++)
+        {
+            if (s[i] == ' ' || i == n)
+            {
+                start =  end + 1;
+                end = i - 1;
+                while(start < end)
+                {
+                    swap(s[start], s[end]);
+                    start++; end--;
+                }
+                end = i;
+                
+                if(i != n && s[i+1]==' ') 
+                {
+                    s.erase(s.begin() + i);
+                    i--;
+                }  
+            }
+        }
+        return s;
+    }
+};
+
 int main () {
     
     
