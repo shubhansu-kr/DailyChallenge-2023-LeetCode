4f620969a01d65f2e4b968ba17d08a5aad530c16
Shubhansu kumar <shubhansu2021@gmail.com>
2023-10-14 09:53:18 +0530

    Painting Walls
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 10_October_2023/13_PaintingWalls.cpp
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/painting-the-walls/?envType=daily-question&envId=2023-10-14
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+public:
+    int solve(vector<int>& cost, vector<int>& time, int i, int bank,vector<vector<int>> &dp){
+        if(bank <= 0){
+            return 0;
+        }
+        if(i >= cost.size()){
+            return 1e9;   
+        }
+        if(dp[i][bank] != -1){
+            return dp[i][bank];
+        }
+        else {
+            int notTake = solve(cost, time, i+1, bank,dp);
+            int take = cost[i] + solve(cost, time, i+1, bank -  time[i] -1,dp); 
+            return dp[i][bank] = min(notTake, take);      
+        }
+    }
+
+    int paintWalls(vector<int>& cost, vector<int>& time) {
+        vector<vector<int>> dp(501,vector<int>(501,-1));
+        return solve(cost, time, 0, time.size(),dp);
+    }
+};
+
+int main () {
+    
+    return 0;
+}
