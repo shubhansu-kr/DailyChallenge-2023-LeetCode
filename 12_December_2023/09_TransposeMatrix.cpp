64fe046279ce6bc50d00c2694f9078ba0c2e61e9
shubhansu-kr <shubhansu2021@gmail.com>
2023-12-10 21:43:17 +0530

    Transpose of a matrix

---
+++ 12_December_2023/09_TransposeMatrix.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/transpose-matrix/?envType=daily-question&envId=2023-12-10
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        vector<vector<int>> ans(matrix[0].size(), vector<int>(matrix.size()));
+        for (int i = 0; i < matrix.size(); i++)
+        {
+            for (int j = 0; j < matrix[0].size(); ++j)
+            {
+                ans[j][i] = matrix[i][j];
+            }
+        }
+        return ans;
+    }
+};
+
+int main () {
+    
+    return 0;
+}
