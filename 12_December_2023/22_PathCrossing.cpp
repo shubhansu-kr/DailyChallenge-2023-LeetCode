4d5193c41606f13dc3ba94cd41278b9da1a93d1b
shubhansu-kr <shubhansu2021@gmail.com>
2023-12-23 22:57:19 +0530

    Path crossing

---
+++ 12_December_2023/22_PathCrossing.cpp
@@ -0,0 +1,40 @@
+// https://leetcode.com/problems/path-crossing/?envType=daily-question&envId=2023-12-23
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+struct pair_hash {
+    template <class T1, class T2>
+    std::size_t operator () (const std::pair<T1, T2>& p) const {
+        auto h1 = std::hash<T1>{}(p.first);
+        auto h2 = std::hash<T2>{}(p.second);
+
+        return h1 ^ h2;
+    }
+};
+
+class Solution {
+public:
+    bool isPathCrossing(std::string path) {
+        int x = 0, y = 0;
+        std::unordered_set<std::pair<int, int>, pair_hash> visited;
+        visited.insert({0, 0});
+
+        for (char direction : path) {
+            x += (direction == 'E') ? 1 : ((direction == 'W') ? -1 : 0);
+            y += (direction == 'N') ? 1 : ((direction == 'S') ? -1 : 0);
+
+            if (visited.find({x, y}) != visited.end()) {
+                return true;
+            }
+
+            visited.insert({x, y});
+        }
+
+        return false;
+    }
+};
+int main () {
+    
+    return 0;
+}
