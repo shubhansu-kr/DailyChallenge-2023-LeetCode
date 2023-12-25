788439fcedd60cf39ad9906f9f29f5c093898951
shubhansu-kr <shubhansu2021@gmail.com>
2023-12-25 21:13:34 +0530

    Decoded ways

---
+++ 12_December_2023/24_DecodeWays.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/decode-ways/?envType=daily-question&envId=2023-12-25
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+public:
+    int numDecodings(string s) {
+        int p = 1, pp, n = s.size();
+        for(int i=n-1;i>=0;i--) {
+            int cur = s[i]=='0' ? 0 : p;
+            if(i<n-1 && (s[i]=='1'||s[i]=='2'&&s[i+1]<'7')) cur+=pp;
+            pp = p;
+            p = cur;
+        }
+        return s.empty()? 0 : p;   
+    }
+};
+
+int main () {
+    
+    return 0;
+}
