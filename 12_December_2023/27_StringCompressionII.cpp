dfd5d01a944dd40ba58db9a227778998e90cb591
shubhansu-kr <shubhansu2021@gmail.com>
2023-12-28 23:02:46 +0530

    Find the best compression

---
+++ 12_December_2023/27_StringCompressionII.cpp
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/string-compression-ii/?envType=daily-question&envId=2023-12-28
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+    const static int N = 127;
+    int dp[N][N];
+    string str;
+    int n;
+    inline int xs(int x) { return x == 1 ? 0 : x < 10 ? 1 : x < 100 ? 2 : 3; }
+    int solve(int left, int k) {
+        if(k < 0) return N;  
+        if(left >= n or n - left <= k) return 0;  
+        int& res = dp[left][k];
+        if(res != -1) return res;
+        res = N;
+        int cnt[26] = {0};
+        for(int j = left, most = 0; j < n; j++) {
+            most = max(most, ++cnt[str[j] - 'a']);  // most = max(count(s[left:j])
+            res = min(res, 1 + xs(most) + solve(j + 1, k - (j - left + 1 - most)));
+        }
+        return res;
+    }
+public:
+    int getLengthOfOptimalCompression(string s, int k) {
+        memset(dp, -1, sizeof(dp));
+        str = s;
+        n = s.size();
+        return solve(0, k);
+    }
+};
+
+int main () {
+    
+    return 0;
+}
